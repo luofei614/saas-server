17906b04acedf3dfb5056ad1888ec5eb37478e7b
luofei614 <upfy@qq.com>
2013-01-07 17:57:06 +0800

    删除多余文件，修改数据库

--- store/api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Test{
-	private function func(){
-	
-	}
-}
-$t=new Test();
-var_dump(method_exists($t,'func'));
--- store/app_repos.sql
+++ store/app_repos.sql
@@ -1,28 +1,32 @@
--- MySQL dump 10.13  Distrib 5.5.28, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: app_repos
--- ------------------------------------------------------
--- Server version	5.5.28-0ubuntu0.12.10.2
+-- 主机: localhost
+-- 生成日期: 2013 年 01 月 07 日 17:52
+-- 服务器版本: 5.5.24
+-- PHP 版本: 5.3.10-1ubuntu3.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `apps`
+-- 数据库: `app_repos`
 --
 
-DROP TABLE IF EXISTS `apps`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `apps` (
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `apps`
+--
+
+CREATE TABLE IF NOT EXISTS `apps` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(100) NOT NULL,
   `src` varchar(150) NOT NULL DEFAULT '' COMMENT 'URL of the app package',
@@ -35,82 +39,66 @@ CREATE TABLE `apps` (
   `disk` int(10) unsigned NOT NULL DEFAULT '100' COMMENT 'Minimum disk space needed to run this app. In MB.',
   `cpu` int(11) NOT NULL DEFAULT '1',
   `version` char(15) NOT NULL,
-  `access` enum('PUBLIC','PRIVATE') NOT NULL DEFAULT 'PUBLIC',
+  `access` tinyint(4) unsigned NOT NULL DEFAULT '0',
   `install_times` int(10) unsigned NOT NULL DEFAULT '0',
-  `liscense` char(10) NOT NULL DEFAULT '''GPL''',
+  `license` char(10) NOT NULL DEFAULT '''GPL''',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `apps`
+-- 转存表中的数据 `apps`
 --
 
-LOCK TABLES `apps` WRITE;
-/*!40000 ALTER TABLE `apps` DISABLE KEYS */;
-/*!40000 ALTER TABLE `apps` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `apps` (`id`, `name`, `src`, `icon`, `cat`, `description`, `runtime`, `services`, `mem`, `disk`, `cpu`, `version`, `access`, `install_times`, `license`) VALUES
+(1, 'wordpress', 'http://wordpress.org', 'http://wordpress.org/icon.png', 'blog', 'wordpress', 'PHP-5.3', 'mysql', 32, 100, 1, '5.2', 0, 0, '''GPL'''),
+(2, 'drupal', 'http://drupal.org', 'http://drupal.org/icon.png', 'cms', 'drupal', 'PHP-5.3', 'mysql', 32, 100, 1, '1.0', 0, 0, '''GPL''');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `config`
+-- 表的结构 `config`
 --
 
-DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `config` (
+CREATE TABLE IF NOT EXISTS `config` (
   `opt` char(40) NOT NULL DEFAULT '',
   `val` varchar(4096) NOT NULL DEFAULT '',
   PRIMARY KEY (`opt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `config`
+-- 转存表中的数据 `config`
 --
 
-LOCK TABLES `config` WRITE;
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` VALUES ('description','a:4:{s:5:\"title\";s:19:\"Demo app repository\";s:7:\"contact\";s:16:\"demo@example.com\";s:4:\"icon\";s:55:\"http://sae.sina.com.cn/static/image/store/createapp.png\";s:7:\"src-url\";s:29:\"http://repos.lajipk.com/apps/\";}'),('repo-ver','-1');
-/*!40000 ALTER TABLE `config` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `config` (`opt`, `val`) VALUES
+('description', 'a:4:{s:5:"title";s:19:"Demo app repository";s:7:"contact";s:16:"demo@example.com";s:4:"icon";s:55:"http://sae.sina.com.cn/static/image/store/createapp.png";s:7:"src-url";s:29:"http://repos.lajipk.com/apps/";}'),
+('repo-ver', '-1');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- 表的结构 `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `password` char(40) NOT NULL,
+  `level` tinyint(4) unsigned NOT NULL DEFAULT '0',
   `akey` char(32) NOT NULL,
   `skey` char(40) NOT NULL,
   `telphone` char(15) NOT NULL,
   `company` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- 转存表中的数据 `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user` (`id`, `name`, `password`, `level`, `akey`, `skey`, `telphone`, `company`) VALUES
+(1, 'luofei', '123456', 0, 'aaa', 'bbb', '186123456', 'sina');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-01-06 10:29:54
